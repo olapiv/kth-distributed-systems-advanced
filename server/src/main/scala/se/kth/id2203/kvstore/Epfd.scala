f2b7f014cc2a444b86c61649aabeebb9cd6d2684
Goradux <abbbo@mail.ru>
2020-02-27 19:25:47 +0100

    Refactored EPFD

--- server/src/main/scala/se/kth/id2203/kvstore/Epfd.scala
+++ server/src/main/scala/se/kth/id2203/kvstore/Epfd.scala
@@ -20,29 +20,24 @@ class EventuallyPerfectFailureDetector extends Port {
   request[StartDetector];
 }
 
-//Define EPFD Implementation
 class EPFD(epfdInit: Init[EPFD]) extends ComponentDefinition {
 
-  //EPFD subscriptions
   val timer = requires[Timer];
   val pLink = requires[Network];
   val epfd = provides[EventuallyPerfectFailureDetector];
 
-  // EPDF component state and initialization
 
-  //configuration parameters
   val self = epfdInit match {case Init(s: NetAddress) => s};
-  var myPartitionTopology: List[NetAddress] = List.empty;
+  var currentTopology: List[NetAddress] = List.empty;
   var systemTopology: Option[LookupTable] = None
 
-  val delta = cfg.getValue[Long]("id2203.project.failureDetectorInterval");
-  //mutable state
-  var period = cfg.getValue[Long]("id2203.project.failureDetectorInterval");
+  val delta = 2000;
+  var period = 2000;
+
   var alive = Set[NetAddress]();
   var suspected = Set[NetAddress]();
   var seqnum = 0;
-  var debugSource : Option[NetAddress] = None
-  var responding : Boolean = true
+  var debugSource: Option[NetAddress] = None
 
   def startTimer(delay: Long): Unit = {
     val scheduledTimeout = new ScheduleTimeout(period);
@@ -56,28 +51,19 @@ class EPFD(epfdInit: Init[EPFD]) extends ComponentDefinition {
         period += delta;
       }
       seqnum = seqnum + 1;
-      for (p <- myPartitionTopology) {
+      for (p <- currentTopology) {
         if (!alive.contains(p) && !suspected.contains(p)) {
           suspected += p;
-
-          if(responding){
-            println(s"$self EFPD suspected $p")
-            trigger(Suspect(p) -> epfd)
-            // send suspect to debug source
-            if(debugSource.isDefined)
-              trigger(NetMessage(self, debugSource.get, Suspect(p)) -> pLink)
-          }
+          println(s"$self EFPD suspected $p")
+          trigger(Suspect(p) -> epfd)
+          if(debugSource.isDefined)
+            trigger(NetMessage(self, debugSource.get, Suspect(p)) -> pLink)
         } else if (alive.contains(p) && suspected.contains(p)) {
           suspected = suspected - p;
-          if(responding){
             println(s"$self EFPD restored $p")
             trigger(Restore(p) -> epfd);
-          }
         }
-        if(responding){
           trigger(NetMessage(self, p, HeartbeatRequest(seqnum)) -> pLink);
-        }
-
       }
       alive = Set[NetAddress]();
       startTimer(period);
@@ -85,20 +71,7 @@ class EPFD(epfdInit: Init[EPFD]) extends ComponentDefinition {
   }
 
   pLink uponEvent {
-
-    case NetMessage(src, deb @Debug("Suicide",_,_)) => {
-      responding = false
-      println(src)
-      println(deb)
-      println(self + " WILL STOP RESPONDING...")
-      println()
-      println()
-    }
-    case NetMessage(_, Debug("FailureDetect", source, _)) => {
-      debugSource = Some(source)
-    }
-
-    case NetMessage(src, HeartbeatRequest(seq)) if responding => {
+    case NetMessage(src, HeartbeatRequest(seq)) => {
       trigger(NetMessage(self, src.src, HeartbeatReply(seq)) -> pLink);
     }
     case NetMessage(src, HeartbeatReply(seq)) => {
@@ -109,11 +82,10 @@ class EPFD(epfdInit: Init[EPFD]) extends ComponentDefinition {
 
   epfd uponEvent {
     case StartDetector(lookupTable: Option[LookupTable], nodes: Set[NetAddress]) => {
-      // start detector for the nodes in particular partition
       systemTopology = lookupTable
-      myPartitionTopology = nodes.toList;
+      currentTopology = nodes.toList;
       suspected = Set[NetAddress]();
-      alive = myPartitionTopology.toSet
+      alive = currentTopology.toSet
       startTimer(period);
     }
   }
